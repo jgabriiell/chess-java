41fc024d5b7b97aca826bcd3165cd59cb9701e66
João Gabriel <jgabriiell@gmail.com>
2025-12-15 20:52:51 -0300

    First class: Position

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
+        Position position = new Position(1, 2);
 
+        System.out.println(position);
     }
 }
